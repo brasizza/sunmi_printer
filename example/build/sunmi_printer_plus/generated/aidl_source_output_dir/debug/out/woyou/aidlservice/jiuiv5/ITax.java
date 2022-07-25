5c10083c713b0da4261dcd3eac073530ee820504
FrenkyDema <frenkydema@gmail.com>
2022-07-25 17:03:33 +0200

    Print working
    missing the <queries> tag in AndroidManifest
    
    add some .Idea staff for debugging

---
+++ example/build/sunmi_printer_plus/generated/aidl_source_output_dir/debug/out/woyou/aidlservice/jiuiv5/ITax.java
@@ -0,0 +1,125 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ */
+package woyou.aidlservice.jiuiv5;
+/**
+ * 打印服务执行结果的回调
+ */
+public interface ITax extends android.os.IInterface
+{
+  /** Default implementation for ITax. */
+  public static class Default implements woyou.aidlservice.jiuiv5.ITax
+  {
+    @Override public void onDataResult(byte[] data) throws android.os.RemoteException
+    {
+    }
+    @Override
+    public android.os.IBinder asBinder() {
+      return null;
+    }
+  }
+  /** Local-side IPC implementation stub class. */
+  public static abstract class Stub extends android.os.Binder implements woyou.aidlservice.jiuiv5.ITax
+  {
+    private static final java.lang.String DESCRIPTOR = "woyou.aidlservice.jiuiv5.ITax";
+    /** Construct the stub at attach it to the interface. */
+    public Stub()
+    {
+      this.attachInterface(this, DESCRIPTOR);
+    }
+    /**
+     * Cast an IBinder object into an woyou.aidlservice.jiuiv5.ITax interface,
+     * generating a proxy if needed.
+     */
+    public static woyou.aidlservice.jiuiv5.ITax asInterface(android.os.IBinder obj)
+    {
+      if ((obj==null)) {
+        return null;
+      }
+      android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+      if (((iin!=null)&&(iin instanceof woyou.aidlservice.jiuiv5.ITax))) {
+        return ((woyou.aidlservice.jiuiv5.ITax)iin);
+      }
+      return new woyou.aidlservice.jiuiv5.ITax.Stub.Proxy(obj);
+    }
+    @Override public android.os.IBinder asBinder()
+    {
+      return this;
+    }
+    @Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+    {
+      java.lang.String descriptor = DESCRIPTOR;
+      switch (code)
+      {
+        case INTERFACE_TRANSACTION:
+        {
+          reply.writeString(descriptor);
+          return true;
+        }
+        case TRANSACTION_onDataResult:
+        {
+          data.enforceInterface(descriptor);
+          byte[] _arg0;
+          _arg0 = data.createByteArray();
+          this.onDataResult(_arg0);
+          return true;
+        }
+        default:
+        {
+          return super.onTransact(code, data, reply, flags);
+        }
+      }
+    }
+    private static class Proxy implements woyou.aidlservice.jiuiv5.ITax
+    {
+      private android.os.IBinder mRemote;
+      Proxy(android.os.IBinder remote)
+      {
+        mRemote = remote;
+      }
+      @Override public android.os.IBinder asBinder()
+      {
+        return mRemote;
+      }
+      public java.lang.String getInterfaceDescriptor()
+      {
+        return DESCRIPTOR;
+      }
+      @Override public void onDataResult(byte[] data) throws android.os.RemoteException
+      {
+        android.os.Parcel _data = android.os.Parcel.obtain();
+        try {
+          _data.writeInterfaceToken(DESCRIPTOR);
+          _data.writeByteArray(data);
+          boolean _status = mRemote.transact(Stub.TRANSACTION_onDataResult, _data, null, android.os.IBinder.FLAG_ONEWAY);
+          if (!_status && getDefaultImpl() != null) {
+            getDefaultImpl().onDataResult(data);
+            return;
+          }
+        }
+        finally {
+          _data.recycle();
+        }
+      }
+      public static woyou.aidlservice.jiuiv5.ITax sDefaultImpl;
+    }
+    static final int TRANSACTION_onDataResult = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+    public static boolean setDefaultImpl(woyou.aidlservice.jiuiv5.ITax impl) {
+      // Only one user of this interface can use this function
+      // at a time. This is a heuristic to detect if two different
+      // users in the same process use this function.
+      if (Stub.Proxy.sDefaultImpl != null) {
+        throw new IllegalStateException("setDefaultImpl() called twice");
+      }
+      if (impl != null) {
+        Stub.Proxy.sDefaultImpl = impl;
+        return true;
+      }
+      return false;
+    }
+    public static woyou.aidlservice.jiuiv5.ITax getDefaultImpl() {
+      return Stub.Proxy.sDefaultImpl;
+    }
+  }
+  public void onDataResult(byte[] data) throws android.os.RemoteException;
+}
---
+++ example/build/sunmi_printer_plus/generated/source/buildConfig/debug/br/com/brasizza/sunmi_printer_plus/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.brasizza.sunmi_printer_plus;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "br.com.brasizza.sunmi_printer_plus";
+  public static final String BUILD_TYPE = "debug";
+}
